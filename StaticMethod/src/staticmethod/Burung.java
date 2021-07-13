b2aa8446f931159c852890fe08514844ed078ca0
JosuaPardosi <josua1811@gmail.com>
2021-07-13 14:43:50 +0700

    first

---
+++ StaticMethod/src/staticmethod/Burung.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package staticmethod;
+
+/**
+ *
+ * @author Kenji
+ */
+public  class Burung {
+    
+    static int usia = 100;
+    public static void suara()
+    {
+        
+        System.out.println("Cuit cuit cuit cuit");
+   
+    }
+}
